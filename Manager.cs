1a9f8c5ca93e57e3a3dac4f490950f316974cdbe
cksuwjr <63836325+cksuwjr@users.noreply.github.com>
2020-11-03 21:39:11 +0900

    1초마다 자동으로 프리팹 생성 및 초당 프레임 계산 코드

---
+++ Manager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    float Cooltime;
+    int Frame;
+    float Second; // 초당 프레임세려고 만듦
+    public GameObject Cube;
+    private void Awake()
+    {
+        Frame = 0;
+        Instantiate(Cube, new Vector3(0, 1, 0), Quaternion.identity);
+        Cooltime = 1.15f;
+    }
+    void Update()
+    {
+        Frame++;
+        if (Second > 0)
+            Second -= Time.deltaTime;
+        else
+        {
+            Debug.Log("현재 초당 프레임:" + Frame);
+            Second = 1.0f;
+            Frame = 0;
+        }
+
+        if (Cooltime > 0)
+            Cooltime -= Time.deltaTime;
+        else
+        {
+            Instantiate(Cube, new Vector3(0, 7f, 0), Quaternion.identity);
+            Cooltime = 1.15f;
+        }
+    }
+}
