45e4765cb892c0dad07b8fb0da63d3e91233f6f0
Agratos <agratos318@naver.com>
2020-11-11 18:34:14 +0900

    JaeYong_Map

---
+++ Assets/ItemCan.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCan : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            PlayerBall player = other.GetComponent<PlayerBall>();
+            player.itemCount++;
+            gameObject.SetActive(false);
+        }
+    }
+}
